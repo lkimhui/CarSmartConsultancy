bea0e40512ebde1d6728238fa9df851536021b1e
Ivanjy11 <114650591+Ivanjy11@users.noreply.github.com>
2023-04-07 13:32:27 +0800

    upload new file with 8822 rows.

--- SmartCard_RecommenderSys.py
+++ SmartCard_RecommenderSys.py
@@ -10,20 +10,13 @@ Created on Wed Apr  5 19:53:27 2023
 import pandas as pd
 
 #Load the CSV file
-df = pd.read_csv('/Users/ivanong/Documents/GitHub/CarSmart/df_all.csv')
-
+df = pd.read_csv('/Users/ivanong/Documents/GitHub/CarSmartConsultancy/sgcarmart_usedcar_info.csv')
+                        
 #preview the csv file
 print(df.describe())
 print(df.info())
 print(df.head())
 
-# clean punctuations 
-
-# Remove punctuation
-df['coe'] = df['coe'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
-
-# Try Cross Tabulation (Car type vs. Power)
-pd.crosstab(df['type'],df['power'])
 
 
 
