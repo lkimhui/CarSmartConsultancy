4f1fd5af5481f21ca9391113d9a30999b5879105
Ivanjy11 <114650591+Ivanjy11@users.noreply.github.com>
2023-04-16 14:08:19 +0800

    commit

--- SmartCard_RecommenderSys.py
+++ SmartCard_RecommenderSys.py
@@ -19,7 +19,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from surprise import Dataset
 from surprise import Reader
 from surprise import SVD
-from surprise.model_selection import cross_validate
+from surprise.model_selection import cross_validate, train_test_split
 from surprise import accuracy
 
 #Load the CSV file
@@ -129,77 +129,107 @@ print(features_cleaned.head())
 #    top_k_items_df = top_k_items_df.sort_values(by='score', ascending=False)
 #    return top_k_items_df.head(k)
 
-df['price'] = df['price'].str.replace(',', '')
-df['price'] = df['price'].str.replace('$', '')
-df['price'] = df['price'].replace('N.A', np.nan)
-df['price'] = df['price'].astype(float)
+
 
 #Building a few sample recommender system
 df = df_nonull
 print(df.head())
 print(df.info())
 
+df['price'] = df['price'].str.replace(',', '')
+df['price'] = df['price'].str.replace('$', '')
+df['price'] = df['price'].replace('N.A', np.nan)
+df['price'] = df['price'].astype(float)
+
 #1 Using just the features price
-#Surprise package
+
 price_df = df[['car_id', 'model', 'price']]
+price_df['price'] = price_df.groupby('model')['price'].transform(lambda x: x.fillna(x.mean()))
+price_df = price_df.dropna(subset=['price'])
+null_counts = price_df.isnull().sum()
+print(null_counts)
 print(price_df.head())
+print(price_df.info())
 
-reader = Reader(rating_scale=(min(price_df['price']), max(price_df['price'])))
-data = Dataset.load_from_df(price_df, reader)
+# Define the Reader object to read the data
+reader = Reader(rating_scale=(price_df['price'].min(), price_df['price'].max()))
 
+# Load the data from the dataframe into the Surprise Dataset object
+data = Dataset.load_from_df(price_df[['model', 'car_id', 'price']], reader=reader)
+
+# Define the SVD algorithm and fit the training data
 algo = SVD()
 trainset = data.build_full_trainset()
 algo.fit(trainset)
 
-user_price = float(input('50000: '))
-item_ids = price_df[(price_df['price'] >= user_price * 0.9) & (price_df['price'] <= user_price * 1.1)]['car_id'].tolist()
+# Define a function to get recommended products
+def get_recommendations(price):
+    # Create a list of items that the user has not yet rated
+    model_list = price_df[price_df['price'] <= price]['model'].unique().tolist()
+    all_models = price_df['model'].unique().tolist()
+    unrated_models = list(set(all_models) - set(model_list))
+    unrated_ids = price_df[price_df['model'].isin(unrated_models)]['car_id'].unique().tolist()
 
-# make predictions for all items within price range
-predicted_ratings = []
-for item_id in item_ids:
-    predicted_rating = algo.predict(1, item_id).est
-    predicted_ratings.append((item_id, predicted_rating))
+    # Create a list of tuples containing the unrated items and their predicted ratings
+    predictions = []
+    for car_id in unrated_ids:
+        prediction = algo.predict(uid='user', iid=car_id)
+        predictions.append((prediction.iid, prediction.est))
 
-# sort predicted ratings in descending order and select top 5 items
-top_items = sorted(predicted_ratings, key=lambda x: x[1], reverse=True)[:5]
+    # Sort the predictions by rating (highest first) and return the top 5
+    predictions.sort(key=lambda x: x[1], reverse=True)
+    top_predictions = predictions[:5]
 
-# display top 5 recommended items
-for item_id, predicted_rating in top_items:
-    item_name = price_df.loc[price_df['car_id'] == item_id, 'model'].iloc[0]
-    print(f'Recommended item: {item_name}, Predicted Rating: {predicted_rating}')
+    # Map the item IDs to their corresponding titles and return the result
+    recommendations = []
+    for car_id, rating in top_predictions:
+        model = price_df[price_df['car_id'] == car_id]['model'].values[0]
+        recommendations.append((model, rating))
+    return recommendations
 
+# Example usage given budget of 50000
+price = 50000
+recommendations = get_recommendations(price)
+print(f"Recommended models within price range of {price}:")
+for rec in recommendations:
+    print(f"Model: {rec[0]}, Predicted Rating: {rec[1]}")
+    
+#Evaluating the recommender 1
 
-#cosine similarity
-# Calculate the similarity matrix based on price
+# Split the data into a training set and a test set
+trainset, testset = train_test_split(data, test_size=0.2, random_state=42)
 
-# Replace NaNs in the price column with median price
-median_price = price_df['price'].median()
-price_df['price'] = price_df['price'].fillna(median_price)
+# Define the SVD algorithm
+algo = SVD()
 
-# Normalize the price column to have zero mean and unit variance
-price_df['price'] = (price_df['price'] - price_df['price'].mean()) / price_df['price'].std()
+# Perform k-fold cross-validation on the training set
+results = cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=False)
 
-# Compute cosine similarity matrix using the normalized price column
-price_sim = cosine_similarity(price_df['price'].values.reshape(1, -1), price_df['price'].values.reshape(1, -1))
+# Print the mean RMSE and MAE scores across all folds
+print(f"Mean RMSE: {results['test_rmse'].mean():.3f}")
+print(f"Mean MAE: {results['test_mae'].mean():.3f}")
 
-# Get the user's specified price range
-min_price = float(input('0: '))
-max_price = float(input('50000: '))
+#The output means that the mean RMSE (Root Mean Squared Error) of the model is 2090304.553, and the mean MAE (Mean Absolute Error) is 1791902.207.
+#The RMSE is a measure of the difference between the predicted values and the actual values. The lower the RMSE value, the better the model's performance. In this case, the RMSE value is quite high, indicating that the model may not be performing well.
+#The MAE is another measure of the difference between the predicted values and the actual values. It is the average of the absolute differences between the predicted values and the actual values. The lower the MAE value, the better the model's performance. In this case, the MAE value is also quite high, indicating that the model may not be performing well.
+#Overall, these evaluation metrics suggest that the model may need further improvement or fine-tuning to achieve better performance.
 
-# Get the items that have a price within the user's specified range
-range_df = price_df[(df['price'] >= min_price) & (price_df['price'] <= max_price)]
+    
+#2 Using all features less text from features, accessories and descriptions
+df_allfeatureslesswords = df
 
-# Calculate the similarity score for each item in the range
-range_sim = cosine_similarity(range_df['price'].values.reshape(1,-1), price_df['price'].values.reshape(1,-1))[0]
+df_allfeatureslesswords['price'] = df_allfeatureslesswords.groupby('model')['price'].transform(lambda x: x.fillna(x.mean()))
+df_allfeatureslesswords = df_allfeatureslesswords.dropna(subset=['price'])
 
-# Sort the items by similarity score and return the top 10 recommendations
-rec_df = price_df.iloc[range_sim.argsort()[::-1][:10]]
+null_counts = df_allfeatureslesswords.isnull().sum()
+print(null_counts)
 
-print(rec_df[['car_id', 'model', 'price']])
-
-#2 Using all features less words
-
-
+df_allfeatureslesswords = df_allfeatureslesswords.drop(['car_features', 'car_accessories', 'descriptions'], axis = 1)
+df_allfeatureslesswords['depreciation', 'road_tax'] = df_allfeatureslesswords['depreciation', 'road_tax'].str.replace(',', '')
+df_allfeatureslesswords['price'] = df_allfeatureslesswords['price'].str.replace('$', '')
+columns_to_float = ['depreciation', 'mileage', 'road_tax', 'deregistration_value', 'coe', 'engine_cap', 'curb_weight', 'trasmission', 'omv', 'arf', 'power', 'number_of_owner']
+df_allfeatureslesswords[columns_to_float] = df_allfeatureslesswords[columns_to_float].astype(float)
+df_allfeatureslesswords.info()
 
 #3 Using just the words description
 
