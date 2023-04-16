e6bb493e3466373877c081c6be87cab6f72a4ded
Ivanjy11 <114650591+Ivanjy11@users.noreply.github.com>
2023-04-16 15:45:34 +0800

    Data Cleaned

--- SmartCard_RecommenderSys.py
+++ SmartCard_RecommenderSys.py
@@ -21,6 +21,7 @@ from surprise import Reader
 from surprise import SVD
 from surprise.model_selection import cross_validate, train_test_split
 from surprise import accuracy
+import datetime
 
 #Load the CSV file
 df = pd.read_csv('/Users/ivanong/Documents/GitHub/CarSmartConsultancy/data/sgcarmart_usedcar_info.csv')
@@ -62,76 +63,7 @@ print(df_nonull.head())
 print(df_nonull.describe())
 print(df_nonull.info())  #Note to kwan, I will use this part for the price recommender
 
-features = df_nonull[['car_id','manufactured_year','mileage']]
-print(features.describe())
-print(features.info())
-print(features.head())
-
-#Data Cleaning 
-features_cleaned = features.copy()
-
-#Delimit Mileage to obtain only in km e.g., 124000
-print(features['mileage'].dtype)
-
-# Define a function to extract the integer mileage value
-def extract_mileage(mileage_str):
-    if mileage_str == 'N.A.':
-        return None
-    else:
-        return int(mileage_str.replace(',', '').split()[0])
-
-# Apply the function to the 'mileage' column
-features_cleaned['mileage_int'] = features_cleaned['mileage'].apply(extract_mileage)
-print(features_cleaned.head())
-
-features_cleaned = features_cleaned[features_cleaned['mileage'] != 'N.A.']
-features_cleaned.drop('mileage', axis=1, inplace=True)
-
-top_50_cars_safety = features_cleaned.sort_values(['manufactured_year', 'mileage_int'], ascending=[False, True]).head(50)
-print(top_50_cars_safety.head())
-#11Apr: yay, able to get the top 50 cars with car_id based on manufactured_year and mileage.
-
-#Remove punctuation from Price and Depreciation
-#features_cleaned['price']= features_cleaned['price'].str.replace('[\$,]', '', regex=True)
-#features_cleaned['price']= features_cleaned['price'].str.replace(',', '', regex=True)
-#features_cleaned['depreciation']= features_cleaned['depreciation'].str.replace('[\$,]', '', regex=True)
-#features_cleaned['depreciation']= features_cleaned['depreciation'].str.replace(',', '', regex=True)
-
-#Remove N.A. 
-# Replace "N.A." values with NaN
-features_cleaned.replace('N.A', pd.NA, inplace=True)
-# Calculate the median value for each column
-median_values = features_cleaned.median()
-# Fill NaN values with median value for each column
-features_cleaned.fillna(median_values, inplace=True)
-
-features_cleaned.sort_values('price', ascending=False, inplace=True)
-print(features_cleaned.head())
-
-#Normalize data to avoid bias towards any attribute
-#features_normalized = (features_cleaned - features_cleaned.mean()) / features_cleaned.std()
-#print(features_normalized.head())
-# Error in normalizing data; need to check further
-
-# Compute the pairwise cosine similarity between the items
-#item_similarities = cosine_similarity(features_cleaned.T)
-
-# Get the top k most similar items for each item
-#k = 5
-#top_k_similar_items = item_similarities.argsort()[:, :-k-1:-1]
-
-# Define a function to get the top k recommended items for a given item
-#def get_top_k_recommendations(item_id):
-#    top_k_items = top_k_similar_items[item_id]
-#    top_k_items_scores = item_similarities[item_id][top_k_items]
-#    top_k_items_df = features_cleaned.iloc[top_k_items][['price','mileage']]
-#    top_k_items_df['score'] = top_k_items_scores
-#    top_k_items_df = top_k_items_df.sort_values(by='score', ascending=False)
-#    return top_k_items_df.head(k)
-
-
-
-#Building a few sample recommender system
+#Building a sample recommender system for testing
 df = df_nonull
 print(df.head())
 print(df.info())
@@ -214,17 +146,15 @@ print(f"Mean MAE: {results['test_mae'].mean():.3f}")
 #The MAE is another measure of the difference between the predicted values and the actual values. It is the average of the absolute differences between the predicted values and the actual values. The lower the MAE value, the better the model's performance. In this case, the MAE value is also quite high, indicating that the model may not be performing well.
 #Overall, these evaluation metrics suggest that the model may need further improvement or fine-tuning to achieve better performance.
 
-    
-#2 Using all features less text from features, accessories and descriptions
+#after initial recommender 1, it seems that an extensive data cleaning is require. Moving to data cleaning first
+
 df_alllesstext= df
-file_name = "checking.xlsx"
-file_path = '/Users/ivanong/Documents/GitHub/CarSmartConsultancy/data'
-df_alllesstext.to_excel(file_path + file_name, index=False)
 
 #after initial recommender 1, it seems that an extensive data cleaning is require. Moving to data cleaning first
 #depreciation
 df_alllesstext['depreciation'] = df_alllesstext['depreciation'].str.replace(',', '')
 df_alllesstext['depreciation'] = df_alllesstext['depreciation'].str.replace('$', '')
+df_alllesstext['depreciation'] = df_alllesstext['depreciation'].replace('N.A.', np.nan)
 df_alllesstext['depreciation'] = df_alllesstext['depreciation'].replace('N.A', np.nan)
 df_alllesstext['depreciation'] = df_alllesstext['depreciation'].astype(float)
 
@@ -268,17 +198,77 @@ df_alllesstext['mileage'] = df_alllesstext['mileage'].str.replace('km', '')
 df_alllesstext['mileage'] = df_alllesstext['mileage'].replace('N.A.', np.nan)
 df_alllesstext['mileage'] = df_alllesstext['mileage'].astype(float)
 
+#enginecap
+df_alllesstext['engine_cap'] = df_alllesstext['engine_cap'].str.replace(',', '')
+df_alllesstext['engine_cap'] = df_alllesstext['engine_cap'].str.replace('cc', '')
+df_alllesstext['engine_cap'] = df_alllesstext['engine_cap'].replace('N.A.', np.nan)
+df_alllesstext['engine_cap'] = df_alllesstext['engine_cap'].astype(float)
 
-file_name = "checking.xlsx"
-file_path = '/Users/ivanong/Documents/GitHub/CarSmartConsultancy/data'
-df_alllesstext.to_excel(file_path + file_name, index=False)
+#curb_weight
+df_alllesstext['curb_weight'] = df_alllesstext['curb_weight'].str.replace(',', '')
+df_alllesstext['curb_weight'] = df_alllesstext['curb_weight'].str.replace('kg', '')
+df_alllesstext['curb_weight'] = df_alllesstext['curb_weight'].replace('N.A.', np.nan)
+df_alllesstext['curb_weight'] = df_alllesstext['engine_cap'].astype(float)
 
+#power
+df_alllesstext['power'] = df_alllesstext['power'].str.replace(r'\(.*?\)', '', regex=True)
+df_alllesstext['power'] = df_alllesstext['power'].str.replace(',', '')
+df_alllesstext['power'] = df_alllesstext['power'].str.replace('kW', '')
+df_alllesstext['power'] = df_alllesstext['power'].replace('N.A.', np.nan)
+df_alllesstext = df_alllesstext[~df_alllesstext['power'].str.contains('More than 6')]
+df_alllesstext['power'] = df_alllesstext['power'].astype(float)
+
+#registration_dates
+# convert the 'registration_date' column to a datetime object
+df_alllesstext['registration_date'] = pd.to_datetime(df_alllesstext['registration_date'], format='%d-%b-%y')
+# calculate the difference between the registration date and today's date
+today = datetime.datetime.now()
+df_alllesstext['age_of_car'] = (today - df_alllesstext['registration_date']) / pd.Timedelta(days=365)
+# show the updated DataFrame
+print(df_alllesstext)
+
+#manufacturered year
+# calculate the age of the car based on the manufacturer year
+df_alllesstext['manufactured_year'] = df_alllesstext['manufactured_year'].astype(float)
+year = 2023
+df_alllesstext['years_since_launch'] = year - df_alllesstext['manufactured_year']
+
+#Transmission
+# use get_dummies() to one-hot encode the 'trasmission' column
+df_onehot = pd.get_dummies(df_alllesstext['trasmission'], prefix='trasmission')
+# concatenate the one-hot encoded features with the original DataFrame
+df_alllesstext = pd.concat([df_alllesstext, df_onehot], axis=1)
+# drop the original 'transmission' column
+df_alllesstext = df_alllesstext.drop('trasmission', axis=1)
+
+#number_of_owner
+# use get_dummies() to one-hot encode the 'number_of_owner' column
+df_onehot = pd.get_dummies(df_alllesstext['number_of_owner'], prefix='number_of_owner')
+# concatenate the one-hot encoded features with the original DataFrame
+df_alllesstext = pd.concat([df_alllesstext, df_onehot], axis=1)
+# drop the original 'transmission' column
+df_alllesstext = df_alllesstext.drop('number_of_owner', axis=1)
+
+#type
+# use get_dummies() to one-hot encode the 'type' column
+df_onehot = pd.get_dummies(df_alllesstext['type'], prefix='type')
+# concatenate the one-hot encoded features with the original DataFrame
+df_alllesstext = pd.concat([df_alllesstext, df_onehot], axis=1)
+# drop the original 'transmission' column
+df_alllesstext = df_alllesstext.drop('type', axis=1)
+
+# show the updated DataFrame
+print(df_alllesstext)
 
 df_alllesstext.info()
 
-null_counts = df_allfeatureslesswords.isnull().sum()
+null_counts = df_alllesstext.isnull().sum()
 print(null_counts)
 
+#This is for data checking after cleaning
+file_name = "checking.xlsx"
+file_path = '/Users/ivanong/Documents/GitHub/CarSmartConsultancy/data'
+df_alllesstext.to_excel(file_path + file_name, index=False)
 
 
 #3 Using just the words description
@@ -346,7 +336,73 @@ print(top_similar_models)
 print(df.head())
 
 
+#Kwan Code
+features = df_nonull[['car_id','manufactured_year','mileage']]
+print(features.describe())
+print(features.info())
+print(features.head())
 
+#Data Cleaning 
+features_cleaned = features.copy()
+
+#Delimit Mileage to obtain only in km e.g., 124000
+print(features['mileage'].dtype)
+
+# Define a function to extract the integer mileage value
+def extract_mileage(mileage_str):
+    if mileage_str == 'N.A.':
+        return None
+    else:
+        return int(mileage_str.replace(',', '').split()[0])
+
+# Apply the function to the 'mileage' column
+features_cleaned['mileage_int'] = features_cleaned['mileage'].apply(extract_mileage)
+print(features_cleaned.head())
+
+features_cleaned = features_cleaned[features_cleaned['mileage'] != 'N.A.']
+features_cleaned.drop('mileage', axis=1, inplace=True)
+
+top_50_cars_safety = features_cleaned.sort_values(['manufactured_year', 'mileage_int'], ascending=[False, True]).head(50)
+print(top_50_cars_safety.head())
+#11Apr: yay, able to get the top 50 cars with car_id based on manufactured_year and mileage.
+
+#Remove punctuation from Price and Depreciation
+#features_cleaned['price']= features_cleaned['price'].str.replace('[\$,]', '', regex=True)
+#features_cleaned['price']= features_cleaned['price'].str.replace(',', '', regex=True)
+#features_cleaned['depreciation']= features_cleaned['depreciation'].str.replace('[\$,]', '', regex=True)
+#features_cleaned['depreciation']= features_cleaned['depreciation'].str.replace(',', '', regex=True)
+
+#Remove N.A. 
+# Replace "N.A." values with NaN
+features_cleaned.replace('N.A', pd.NA, inplace=True)
+# Calculate the median value for each column
+median_values = features_cleaned.median()
+# Fill NaN values with median value for each column
+features_cleaned.fillna(median_values, inplace=True)
+
+features_cleaned.sort_values('price', ascending=False, inplace=True)
+print(features_cleaned.head())
+
+#Normalize data to avoid bias towards any attribute
+#features_normalized = (features_cleaned - features_cleaned.mean()) / features_cleaned.std()
+#print(features_normalized.head())
+# Error in normalizing data; need to check further
+
+# Compute the pairwise cosine similarity between the items
+#item_similarities = cosine_similarity(features_cleaned.T)
+
+# Get the top k most similar items for each item
+#k = 5
+#top_k_similar_items = item_similarities.argsort()[:, :-k-1:-1]
+
+# Define a function to get the top k recommended items for a given item
+#def get_top_k_recommendations(item_id):
+#    top_k_items = top_k_similar_items[item_id]
+#    top_k_items_scores = item_similarities[item_id][top_k_items]
+#    top_k_items_df = features_cleaned.iloc[top_k_items][['price','mileage']]
+#    top_k_items_df['score'] = top_k_items_scores
+#    top_k_items_df = top_k_items_df.sort_values(by='score', ascending=False)
+#    return top_k_items_df.head(k)
 
 
 
