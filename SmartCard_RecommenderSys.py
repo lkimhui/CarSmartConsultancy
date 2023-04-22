c9aa6070be8a6e0062cbcca08f005375dea2fe13
Ivanjy11 <114650591+Ivanjy11@users.noreply.github.com>
2023-04-22 12:28:42 +0800

    Update SmartCard_RecommenderSys.py

--- SmartCard_RecommenderSys.py
+++ SmartCard_RecommenderSys.py
@@ -29,71 +29,36 @@ from surprise import accuracy
 import datetime
 import tkinter as tk
 
-
-#after initial recommender 1, it seems that an extensive data cleaning is require. Moving to data cleaning first
-#considering cleaned and rolled up data here
-
 ## Adding latest cleaned and rolled up data here ## 
-#df = pd.read_csv('/Users/ivanong/Documents/GitHub/CarSmartConsultancy/Data/cleaned_data/datarollup_latest.csv')
-df = pd.read_csv('/Users/kwanyick/Documents/GitHub/CarSmartConsultancy/Data/cleaned_data/datarollup_latest.csv')
-
-
-##5 Recommender in total ##
-
-#1.) basic, just price range (done)
 
 #Load the CSV file
-df_old = pd.read_csv('/Users/ivanong/Documents/GitHub/CarSmartConsultancy/data/sgcarmart_usedcar_info.csv')
-#df_old = pd.read_csv('/Users/kwanyick/Documents/GitHub/CarSmartConsultancy/data/sgcarmart_usedcar_info.csv')
-    
+
 #preview the csv file
+df = pd.read_csv('/Users/ivanong/Documents/GitHub/CarSmartConsultancy/Data/cleaned_data/datarollup_latest.csv')
+#df = pd.read_csv('/Users/kwanyick/Documents/GitHub/CarSmartConsultancy/Data/cleaned_data/datarollup_latest.csv')
 print(df.describe())
 print(df.info())
 print(df.head())
 
-#Clean data, remove all the Null
-df_nonull = df.dropna()
+##5 Recommender in total ##
 
-# Print the number of rows before and after dropping nulls
-print('Number of rows before dropping nulls:', len(df))
-print('Number of rows after dropping nulls:', len(df_nonull))
-print(df_nonull.head())
+#1.) revisting the 1st model using just price on the clean dataset
 
-#Create data frame with unique ID(new column) and selected attributes
-# Generate a sequence of integers for the unique IDs
-ids = range(1, len(df_nonull)+1)
-df_nonull['car_id']= ids
-col = df_nonull.pop('car_id')
-df_nonull.insert(0,'car_id',col)
-print(df_nonull.head())
-print(df_nonull.describe())
-print(df_nonull.info())  
+df_1 = df
 
-df = df_nonull
-print(df.head())
-print(df.info())
-
-df['price'] = df['price'].str.replace(',', '')
-df['price'] = df['price'].str.replace('$', '')
-df['price'] = df['price'].replace('N.A', np.nan)
-df['price'] = df['price'].astype(float)
-
-#Building a sample recommender system for testing
-#1 Using just the features price
-
-price_df = df[['car_id', 'model', 'price']]
-price_df['price'] = price_df.groupby('model')['price'].transform(lambda x: x.fillna(x.mean()))
-price_df = price_df.dropna(subset=['price'])
-null_counts = price_df.isnull().sum()
+df_1 = df[['car_id', 'model', 'price']]
+df_1['price'] = df_1.groupby('model')['price'].transform(lambda x: x.fillna(x.mean()))
+df_1 = df_1.dropna(subset=['price'])
+null_counts = df_1.isnull().sum()
 print(null_counts)
-print(price_df.head())
-print(price_df.info())
+print(df_1.head())
+print(df_1.info())
 
 # Define the Reader object to read the data
-reader = Reader(rating_scale=(price_df['price'].min(), price_df['price'].max()))
+reader = Reader(rating_scale=(df_1['price'].min(), df_1['price'].max()))
 
 # Load the data from the dataframe into the Surprise Dataset object
-data = Dataset.load_from_df(price_df[['model', 'car_id', 'price']], reader=reader)
+data = Dataset.load_from_df(df_1[['model', 'car_id', 'price']], reader=reader)
 
 # Define the SVD algorithm and fit the training data
 algo = SVD()
@@ -103,10 +68,10 @@ algo.fit(trainset)
 # Define a function to get recommended products
 def get_recommendations(price):
     # Create a list of items that the user has not yet rated
-    model_list = price_df[price_df['price'] <= price]['model'].unique().tolist()
-    all_models = price_df['model'].unique().tolist()
+    model_list = df_1[df_1['price'] <= price]['model'].unique().tolist()
+    all_models = df_1['model'].unique().tolist()
     unrated_models = list(set(all_models) - set(model_list))
-    unrated_ids = price_df[price_df['model'].isin(unrated_models)]['car_id'].unique().tolist()
+    unrated_ids = df_1[df_1['model'].isin(unrated_models)]['car_id'].unique().tolist()
 
     # Create a list of tuples containing the unrated items and their predicted ratings
     predictions = []
@@ -121,7 +86,7 @@ def get_recommendations(price):
     # Map the item IDs to their corresponding titles and return the result
     recommendations = []
     for car_id, rating in top_predictions:
-        model = price_df[price_df['car_id'] == car_id]['model'].values[0]
+        model = df_1[df_1['car_id'] == car_id]['model'].values[0]
         recommendations.append((model, rating))
     return recommendations
 
@@ -147,52 +112,53 @@ results = cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=Fal
 print(f"Mean RMSE: {results['test_rmse'].mean():.3f}")
 print(f"Mean MAE: {results['test_mae'].mean():.3f}")
 
-#The output means that the mean RMSE (Root Mean Squared Error) of the model is 2090304.553, and the mean MAE (Mean Absolute Error) is 1791902.207.
-#The RMSE is a measure of the difference between the predicted values and the actual values. The lower the RMSE value, the better the model's performance. In this case, the RMSE value is quite high, indicating that the model may not be performing well.
-#The MAE is another measure of the difference between the predicted values and the actual values. It is the average of the absolute differences between the predicted values and the actual values. The lower the MAE value, the better the model's performance. In this case, the MAE value is also quite high, indicating that the model may not be performing well.
-#Overall, these evaluation metrics suggest that the model may need further improvement or fine-tuning to achieve better performance.
 
+#The output means that the mean RMSE (Root Mean Squared Error) of the model is 185201.348 vs 2090304.553 previously and the mean MAE (Mean Absolute Error) is 104722.119 vs 1791902.207 previously.
+#This is a stark improvement from the model prior to cleaning in our first try. However, we need to reference further to other model and utilize the other features to compare 
+#We will explore 4 more methods here
 
-#2.) Similarity based on product text descritpion columns (done)
+#2.) Similarity based on product text descritpion columns
 
 #Mining the text data
 #1 Using just the words description to build a recommdner
 
+df_2 = df
+df_2.info()
 #Converting features to string
-df_alllesstext['car_features'] = df_alllesstext['car_features'].astype(str)
-df_alllesstext['car_accessories'] = df_alllesstext['car_accessories'].astype(str)
-df_alllesstext['descriptions'] = df_alllesstext['descriptions'].astype(str)
+df_2['features'] = df_2['features'].astype(str)
+df_2['accessories'] = df_2['accessories'].astype(str)
+df_2['descriptions'] = df_2['descriptions'].astype(str)
 
 
 # define the columns to clean
-cols_to_clean = ['car_features', 'car_accessories', 'descriptions']
+cols_to_clean = ['features', 'accessories', 'descriptions']
 
 # convert the specified columns to lowercase
-df_alllesstext[cols_to_clean] = df_alllesstext[cols_to_clean].apply(lambda x: x.str.lower())
+df_2[cols_to_clean] = df_2[cols_to_clean].apply(lambda x: x.str.lower())
 
 # remove numbers and unwanted characters using regular expressions
-df_alllesstext[cols_to_clean] = df_alllesstext[cols_to_clean].apply(lambda x: x.str.replace(r'[^a-z\s]', '', regex=True))
+df_2[cols_to_clean] = df_2[cols_to_clean].apply(lambda x: x.str.replace(r'[^a-z\s]', '', regex=True))
 
 # tokenize the specified columns into words
-df_alllesstext[cols_to_clean] = df_alllesstext[cols_to_clean].apply(lambda x: x.str.split())
+df_2[cols_to_clean] = df_2[cols_to_clean].apply(lambda x: x.str.split())
 
 # remove stop words using nltk library
 stop_words = set(stopwords.words('english'))
-df_alllesstext[cols_to_clean] = df_alllesstext[cols_to_clean].apply(lambda x: x.apply(lambda y: [word for word in y if not pd.isnull(word) and word not in stop_words]))
+df_2[cols_to_clean] = df_2[cols_to_clean].apply(lambda x: x.apply(lambda y: [word for word in y if not pd.isnull(word) and word not in stop_words]))
 
 # lemmatize the words using nltk library
 lemmatizer = WordNetLemmatizer()
-df_alllesstext[cols_to_clean] = df_alllesstext[cols_to_clean].apply(lambda x: x.apply(lambda y: [lemmatizer.lemmatize(word) for word in y]))
+df_2[cols_to_clean] = df_2[cols_to_clean].apply(lambda x: x.apply(lambda y: [lemmatizer.lemmatize(word) for word in y]))
 
 # join the words back into a single string
-df_alllesstext[cols_to_clean] = df_alllesstext[cols_to_clean].apply(lambda x: x.apply(lambda y: ' '.join(y)))
+df_2[cols_to_clean] = df_2[cols_to_clean].apply(lambda x: x.apply(lambda y: ' '.join(y)))
 
 # fill NaN values with empty string
 #df[cols_to_clean] = df[cols_to_clean].replace(np.nan, '', regex=True)
 
 # vectorize the cleaned up columns using TfidfVectorizer
 vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(df_alllesstext[cols_to_clean].apply(lambda x: ' '.join(x), axis=1))
+X = vectorizer.fit_transform(df_2[cols_to_clean].apply(lambda x: ' '.join(x), axis=1))
 
 # print the resulting feature names and document-term matrix
 print(vectorizer.get_feature_names())
@@ -202,7 +168,7 @@ print(X.toarray())
 # create a dataframe from the resulting document-term matrix
 tfidf_df = pd.DataFrame(X.toarray(),
                         columns=vectorizer.get_feature_names())
-tfidf_df.index = df_alllesstext['model']
+tfidf_df.index = df_2['model']
 
 print(tfidf_df)
 
@@ -218,9 +184,11 @@ top_similar_models = cos_sim_df[[model_name]].sort_values(by=model_name, ascendi
 
 print(top_similar_models)
 
-print(df_alllesstext.head())
+print(df_2.head())
 
 #3.) base on all non text features
+
+df_3 = df
 
 
 #4.) base on combine feature, text and non text (This is for user who dunno what they want)
